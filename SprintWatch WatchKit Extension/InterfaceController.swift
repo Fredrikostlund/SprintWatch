385ff071dfa802a7aede74d3e3ad8b3f4f5c84fd
Fredrik Östlund <id15fod@cs.umu.se>
2018-10-08 14:03:24 +0200

    Initial Commit

---
+++ SprintWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SprintWatch WatchKit Extension
+//
+//  Created by Fredrik Östlund on 2018-10-08.
+//  Copyright © 2018 Fredrik Östlund. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ SprintWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SprintWatch
+//
+//  Created by Fredrik Östlund on 2018-10-08.
+//  Copyright © 2018 Fredrik Östlund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
