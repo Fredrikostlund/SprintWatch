75403a3095244ea4160c0111adf46bd458a064f5
Martin <martinsjolund@hotmail.com>
2018-10-11 10:35:58 +0200

    Changed Timer controller name and changed controller for Timer scene

--- SprintWatch WatchKit Extension/TimerController.swift
+++ SprintWatch WatchKit Extension/TimerController.swift
@@ -10,7 +10,7 @@ import WatchKit
 import Foundation
 
 
-class Timer: WKInterfaceController {
+class TimerController: WKInterfaceController {
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
