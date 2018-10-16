98dc463442610e30baf59106001f64cc2deee4ec
Martin <martinsjolund@hotmail.com>
2018-10-16 11:52:59 +0200

    1 to 60 seconds in available in the set rest picker

--- SprintWatch WatchKit Extension/RestTimeController.swift
+++ SprintWatch WatchKit Extension/RestTimeController.swift
@@ -1,8 +1,8 @@
 //
-//  SettingsController.swift
+//  RestTimeController.swift
 //  SprintWatch WatchKit Extension
 //
-//  Created by Martin Sjölund on 2018-10-11.
+//  Created by Martin Sjölund on 2018-10-12.
 //  Copyright © 2018 Fredrik Östlund. All rights reserved.
 //
 
@@ -10,29 +10,26 @@ import WatchKit
 import Foundation
 
 
-class LapController: WKInterfaceController{
+class RestTimeController: WKInterfaceController{
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-
+        
         setPickerItems()
         // Configure interface objects here.
     }
     
-    //sets the items 1 to 10 to lap picker
     func setPickerItems(){
         var pickerItems: [WKPickerItem] = []
         
-        for i in 1...10{
+        for i in 1...60{
             print(i)
             let item = WKPickerItem()
             item.title = String(i)
             pickerItems.append(item)
         }
-        
-        lapPicker.setItems(pickerItems)
+        restTimePicker.setItems(pickerItems)
     }
-
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
@@ -44,7 +41,7 @@ class LapController: WKInterfaceController{
         super.didDeactivate()
     }
     
-    @IBOutlet weak var lapPicker: WKInterfacePicker!
-    //Label tagged "Next", works as button to next view  
+    @IBOutlet weak var restTimePicker: WKInterfacePicker!
+    @IBOutlet weak var doneLabel: WKInterfaceLabel!
     
 }
