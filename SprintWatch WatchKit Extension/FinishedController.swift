7fa6612461dbc70616b213f1a9a5dba308587f9f
Martin <martinsjolund@hotmail.com>
2018-10-26 14:59:14 +0200

    Added new view and controller. Removed outlet in interface controller

--- SprintWatch WatchKit Extension/FinishedController.swift
+++ SprintWatch WatchKit Extension/FinishedController.swift
@@ -1,18 +1,16 @@
 //
-//  InterfaceController.swift
+//  FinishedController.swift
 //  SprintWatch WatchKit Extension
 //
-//  Created by Fredrik Östlund on 2018-10-08.
+//  Created by Martin Sjölund on 2018-10-26.
 //  Copyright © 2018 Fredrik Östlund. All rights reserved.
 //
-
 
 import WatchKit
 import Foundation
 
-
-class InterfaceController: WKInterfaceController {
-
+class FinishedController: WKInterfaceController {
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
